4b01b529387c94f894c55a30f640a9acc152e323
Aaron Schuman <aaron.schuman@urbanairship.com>
2017-04-06 15:54:20 -0700

    add authentication to needed endpoint

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!bin/python
-
 import json
 import uuid
 import time
@@ -9,12 +7,12 @@ from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 BASE_URL = '/api/v1/contacts'
-contacts = []
+CONTACTS = []
 
 def validate_contact(form_data, user):
     input_data = {}
 
-    input_data['modifying_user'] = user
+    input_data['modified_by'] = user
 
     if 'email' in request.form:
         input_data['email'] = form_data['email']
@@ -40,20 +38,43 @@ def validate_contact(form_data, user):
 
 def email_filter(email_arg):
     filtered = []
-    if contacts:
-        for contact in contacts:
+    
+    if CONTACTS:
+        for contact in CONTACTS:
             if contact[contact.keys()[0]]['email'] == email_arg:
                 filtered.append(contact)
-
     if filtered:
         return filtered
     else:
         abort(404)
 
+def authenticate(request):
+
+    allowed_users = {
+        'user1': 'password1',
+        'user2': 'verysecret',
+        'another_user': 'openplz'
+    }
+
+    if request.authorization:
+        username = request.authorization.username
+        password = request.authorization.password
+
+        if username in allowed_users.keys():
+            if allowed_users[username] == password:
+                return username
+        abort(401)
+    else:
+        abort(401)
+
 # error handlers
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'bad request'}), 400)
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return make_response(jsonify({'error': 'unauthorized'}), 401)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -67,12 +88,12 @@ def list_contacts():
         filtered = email_filter(request.args.get('email'))
         return make_response(jsonify(filtered), 200)
     else:
-        return jsonify(contacts)
+        return jsonify(CONTACTS)
 
 @app.route(BASE_URL + '/<contact_id>/', methods=['GET'])
 def get_contact(contact_id):
-    if contacts:
-        for contact in contacts:
+    if CONTACTS:
+        for contact in CONTACTS:
             if contact_id in contact.keys():
                 return jsonify(contact)
         abort(404)
@@ -82,44 +103,46 @@ def get_contact(contact_id):
 @app.route(BASE_URL, methods=['POST'])
 def add_contact():
     
-    # check auth TODO
-    user = 'luv2add'
+    user = authenticate(request)
 
-    # validate data & build contact
     input_data = validate_contact(request.form, user)
 
-    # create UUID
     new_uuid = uuid.uuid4()
 
-    contacts.append({str(new_uuid) : input_data})
+    CONTACTS.append({str(new_uuid) : input_data})
 
     return make_response(jsonify({'contact_id': str(new_uuid)}))
 
 @app.route(BASE_URL + '/<contact_id>/', methods=['PUT'])
 def replace_contact(contact_id):
     
-    # check auth TODO
-    user = 'luv2change'
+    user = authenticate(request)
 
     input_data = validate_contact(request.form, user)
 
-    if contacts:
-        for contact in contacts:
+    if CONTACTS:
+        for contact in CONTACTS:
             if contact_id in contact.keys():
                 contact[contact_id] = input_data
+
                 return make_response(jsonify({'ok': input_data}), 200)
+        
         abort(404)
     else:
         abort(404)
 
 @app.route(BASE_URL + '/<contact_id>/', methods=['DELETE'])
 def delete_contact(contact_id):
-    if contacts:
-        for contact in contacts:
+
+    authenticate(request)
+
+    if CONTACTS:
+        for contact in CONTACTS:
             if contact_id in contact.keys():
-                # idx = contacts.index(contact)
-                contacts.pop(contacts.index(contact))
+                CONTACTS.pop(CONTACTS.index(contact))
+
                 return make_response(jsonify({'deleted': contact}), 200)
+       
         abort(404)
     else:
         abort(404)
